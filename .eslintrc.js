f63dba860addf510e5c0d2d431bf31c81312193d
Olga_Belaya <protenot@yandex.ru>
2023-08-22 16:03:13 +0300

    install prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,31 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "jest/globals": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/eslint-recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint", "jest", "import"
-    ],
-    "root":true,
-    "rules": {
-        "no-undef":"off",
-        "@typescript-eslint/no-var-requires":"off",
-        
-        "import/prefer-default-export": "off",
-            "import/no-unresolved": "off",
-            "import/extensions": ["warn", "never"],
-            "@typescript-eslint/prefer-nullish-coalescing": "off",
-            "@typescript-eslint/strict-boolean-expressions": "off",
-        
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  extends: [
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "jest", "import"],
+  root: true,
+  rules: {
+    "no-undef": "off",
+    "@typescript-eslint/no-var-requires": "off",
 
-    }
-}
+    "import/prefer-default-export": "off",
+    "import/no-unresolved": "off",
+    "import/extensions": ["warn", "never"],
+    "@typescript-eslint/prefer-nullish-coalescing": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+  },
+};

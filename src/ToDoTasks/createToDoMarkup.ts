0895d763517bf4b36e24f3ad07e13c391adcea7a
Olga_Belaya <protenot@yandex.ru>
2023-08-30 15:29:37 +0300

    fuzzy-search working

--- src/ToDoTasks/createToDoMarkup.ts
+++ src/ToDoTasks/createToDoMarkup.ts
@@ -2,6 +2,7 @@ import { current } from "@reduxjs/toolkit";
 import { Status, ToDoTask } from "./TypesToDo";
 import { ToDoList } from "./classToDo";
 import { createID } from "./createIDToDo";
+import FuzzySearch from "fuzzy-search";
 //newToDoList.createToDoTask()
 
 const newToDoList = new ToDoList();
@@ -18,24 +19,61 @@ const toDoListTitle: string[] = [
 ];
 //console.log(statusVar);
 
-export function createModalW(el: HTMLElement) {
+export async function createModalW(el: HTMLElement) {
   const inputToDos: HTMLInputElement = document.createElement("input");
   // console.log(toDoContainer)
   el.append(inputToDos);
   inputToDos.classList.add("input-todos");
+  inputToDos.id = "lable";
+  inputToDos.setAttribute("list","tasks-choice");
   inputToDos.placeholder = "Введите задачу";
   inputToDos.type = "text";
+  inputToDos.autocomplete = "on"
 
   const inputDate: HTMLInputElement = document.createElement("input");
   el.append(inputDate);
   inputDate.classList.add("input-date");
   inputDate.type = "datetime-local";
 
-  /* let inputStatus:HTMLInputElement = document.createElement("input");
-      toDoContainer.append(inputStatus);
-      inputStatus.classList.add("input-status")
-      inputStatus.type = ""; */
 
+
+// создаем listener для реализации поиска в строке ввода
+
+let inputTasks= Array.from(await newToDoList.getToDoTask());
+console.log(inputTasks)
+let tasksArray =inputTasks.reduce((res,obj)=>{
+    const key:string = obj.content;
+    if(!res[key]){res[key]=[]}
+    res[key].push(obj)
+    return res;
+},{})
+
+tasksArray=Object.keys(tasksArray)
+ 
+console.log(tasksArray)
+
+inputToDos.addEventListener('keyup',()=>{
+    const searcher = new FuzzySearch(tasksArray);
+    const result = searcher.search(inputToDos.value)
+    console.log(result);
+   // result.forEach((elem:string)=>inputToDos.placeholder=elem)
+   let label = document.createElement('label');
+   inputToDos.append(label)
+   label.setAttribute('for', 'label')
+   let dataList = document.createElement('datalist');
+    dataList.id="tasks-choice"
+    inputToDos.setAttribute("list","tasks-choice");
+    inputToDos.name = "label";
+    for (let i = 0; i<result.length; i++){
+        const optionWord = document.createElement("option");
+        optionWord.value = result[i];
+        
+        optionWord.classList.add("tasks-option");
+        dataList.appendChild(optionWord);
+el.append(dataList)
+    }
+console.log(dataList)
+})
   const toDoButton: HTMLButtonElement = document.createElement("button");
   el.append(toDoButton);
   toDoButton.classList.add("main-button");
